45d372da454664eeffcd13128a1b98dd00ea2db4
vinnysoft23@gmail.com <vinnysoft23@gmail.com@261c7f52-1c54-131f-4f96-7bbad5d8ecb7>
2012-12-11 15:57:49 +0000

    Adjustments to obtain the camera at onResume method.

--- trunk/Sample/ForegroundCameraPlugin/src/com/foregroundcameraplugin/CameraActivity.java
+++ trunk/Sample/ForegroundCameraPlugin/src/com/foregroundcameraplugin/CameraActivity.java
@@ -38,99 +38,111 @@ import android.widget.FrameLayout;
  */
 public class CameraActivity extends Activity {
 
-	private static final String TAG = "CameraActivity";
+    private static final String TAG = "CameraActivity";
 
-	private Camera mCamera;
-	private ForegroundCameraPreview mPreview;
-	private boolean pressed = false;
+    private Camera mCamera;
+    private ForegroundCameraPreview mPreview;
+    private FrameLayout mFrameLayout;
+    private boolean pressed = false;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.foregroundcameraplugin);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.foregroundcameraplugin);
 
-		// Create an instance of Camera
-		mCamera = getCameraInstance();
+	// Create an instance of Camera
+	mCamera = getCameraInstance();
 
-		// Create a Preview and set it as the content of activity.
-		mPreview = new ForegroundCameraPreview(this, mCamera);
-		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-		preview.addView(mPreview);
+	// Create a Preview and set it as the content of activity.
+	mPreview = new ForegroundCameraPreview(this, mCamera);
+	mFrameLayout = (FrameLayout) findViewById(R.id.camera_preview);
+	mFrameLayout.addView(mPreview);
 
-		// Add a listener to the Capture button
-		Button captureButton = (Button) findViewById(R.id.button_capture);
-		captureButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
+	// Add a listener to the Capture button
+	Button captureButton = (Button) findViewById(R.id.button_capture);
+	captureButton.setOnClickListener(new View.OnClickListener() {
+	    public void onClick(View v) {
 
-				if (pressed)
-					return;
+		if (pressed)
+		    return;
 
-				// Set pressed = true to prevent freezing.
-				// Issue 1 at
-				// http://code.google.com/p/foreground-camera-plugin/issues/detail?id=1
-				pressed = true;
+		// Set pressed = true to prevent freezing.
+		// Issue 1 at
+		// http://code.google.com/p/foreground-camera-plugin/issues/detail?id=1
+		pressed = true;
 
-				// get an image from the camera
-				mCamera.autoFocus(new AutoFocusCallback() {
+		// get an image from the camera
+		mCamera.autoFocus(new AutoFocusCallback() {
 
-					public void onAutoFocus(boolean success, Camera camera) {
-						mCamera.takePicture(null, null, mPicture);
-					}
-				});
-			}
+		    public void onAutoFocus(boolean success, Camera camera) {
+			mCamera.takePicture(null, null, mPicture);
+		    }
 		});
+	    }
+	});
 
-		Button cancelButton = (Button) findViewById(R.id.button_cancel);
-		cancelButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				pressed = false;
-				setResult(RESULT_CANCELED);
-				finish();
-			}
-		});
+	Button cancelButton = (Button) findViewById(R.id.button_cancel);
+	cancelButton.setOnClickListener(new View.OnClickListener() {
+	    public void onClick(View v) {
+		pressed = false;
+		setResult(RESULT_CANCELED);
+		finish();
+	    }
+	});
+    }
+
+    @Override
+    protected void onPause() {
+	if (mCamera != null) {
+	    mCamera.release(); // release the camera for other applications
+	    mFrameLayout.removeView(mPreview); //Remove the preview from layout.
+	    mCamera = null;
 	}
+	super.onPause();
+    }
 
-	@Override
-	protected void onPause() {
-		if (mCamera != null) {
-			mCamera.release(); // release the camera for other applications
-			mCamera = null;
-		}
-		super.onPause();
+    @Override
+    protected void onResume() {
+	if (mCamera == null) {
+	    mCamera = getCameraInstance(); //get the camera on resume.
+	    mPreview = new ForegroundCameraPreview(this, mCamera); // Create the preview.
+	    mFrameLayout.addView(mPreview); //Add the preview to layout.
 	}
+	super.onResume();
+    }
 
-	/** A safe way to get an instance of the Camera object. */
-	public static Camera getCameraInstance() {
-		Camera c = null;
-		try {
-			c = Camera.open(); // attempt to get a Camera instance
-		} catch (Exception e) {
-			// Camera is not available (in use or does not exist)
-		}
-		return c; // returns null if camera is unavailable
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance() {
+	Camera c = null;
+	try {
+	    c = Camera.open(); // attempt to get a Camera instance
+	} catch (Exception e) {
+	    // Camera is not available (in use or does not exist)
 	}
+	return c; // returns null if camera is unavailable
+    }
 
-	private PictureCallback mPicture = new PictureCallback() {
+    private PictureCallback mPicture = new PictureCallback() {
 
-		public void onPictureTaken(byte[] data, Camera camera) {
+	public void onPictureTaken(byte[] data, Camera camera) {
 
-			Uri fileUri = (Uri) getIntent().getExtras().get(
-					MediaStore.EXTRA_OUTPUT);
+	    Uri fileUri = (Uri) getIntent().getExtras().get(
+		    MediaStore.EXTRA_OUTPUT);
 
-			File pictureFile = new File(fileUri.getPath());
+	    File pictureFile = new File(fileUri.getPath());
 
-			try {
-				FileOutputStream fos = new FileOutputStream(pictureFile);
-				fos.write(data);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				Log.d(TAG, "File not found: " + e.getMessage());
-			} catch (IOException e) {
-				Log.d(TAG, "Error accessing file: " + e.getMessage());
-			}
-			setResult(RESULT_OK);
-			pressed = false;
-			finish();
-		}
-	};
+	    try {
+		FileOutputStream fos = new FileOutputStream(pictureFile);
+		fos.write(data);
+		fos.close();
+	    } catch (FileNotFoundException e) {
+		Log.d(TAG, "File not found: " + e.getMessage());
+	    } catch (IOException e) {
+		Log.d(TAG, "Error accessing file: " + e.getMessage());
+	    }
+	    setResult(RESULT_OK);
+	    pressed = false;
+	    finish();
+	}
+    };
 }
